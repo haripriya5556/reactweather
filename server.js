5d56469a84546ed344582b6b31a3842759c32ac3
haripriya <deepu.sai88@gamil.com>
2016-07-27 15:33:02 +0530

    proc repository

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express=require('express');
 var app=express();
+app.use(express.static(__dirname + '/'));
 
+app.listen(process.env.PORT || 8080);
 
-app.use(express.static('public'));
-app.listen($PORT, function(){
+//app.use(express.static('public'));
+//app.listen($PORT, function(){
  console.log('express server is upon $PORT ');
 });

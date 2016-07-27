a15be10ecca80bbb45740643e6d9aa314c7ac525
haripriya <deepu.sai88@gamil.com>
2016-07-27 18:47:24 +0530

    reactweather

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-var express=require('express');
-var app=express();
-const PORT =process.env.PORT ||3000;
-app.use(function (req, res, next){
-  if(req.headers['x-forwarded-proto']==='http') {
-   next();
-  }
- else{
-   res.redirect('http://'+req.hostname + req.url);
- }
-});
-app.use(express.static('public'));
-app.listen(PORT, function () {
- console.log('express server is upon port '+ PORT);
-});
+//var express=require('express');
+//var app=express();
+//const PORT =process.env.PORT ||3000;
+//app.use(function (req, res, next){
+//  if(req.headers['x-forwarded-proto']==='http') {
+//    next();
+//  }
+//  else{
+//    res.redirect('http://'+req.hostname + req.url);
+//  }
+//});
+//app.use(express.static('public'));
+//app.listen(PORT, function () {
+ //console.log('express server is upon port '+ PORT);
+//});
+var server = require('http').createServer(app).listen(process.env.PORT || 8080);

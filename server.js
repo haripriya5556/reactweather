ac0c0c294d5900713ad97791f7486c83ddce0c1b
haripriya <deepu.sai88@gamil.com>
2016-07-27 16:48:28 +0530

    reactweather

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express=require('express');
 var app=express();
-const PORT =process.env.port ||3000;
+const PORT =process.env.port || 3000;
 app.use(function (req, res, next){
   if(req.headers['x-forwarded-proto']==='http') {
     next();
@@ -8,7 +8,7 @@ app.use(function (req, res, next){
   else{
     res.redirect('http://'+req.hostname + req.url);
   }
-  
+
 });
 //app.use(express.static(__dirname + '/'));
 

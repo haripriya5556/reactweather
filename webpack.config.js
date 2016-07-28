a0c4bf167fb1f94d9c58c30be940557abd12bf45
haripriya <deepu.sai88@gamil.com>
2016-07-28 14:30:34 +0530

    add foundation&jquery

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,18 @@
 var webpack=require('webpack');
 module.exports ={
   entry:[
-
+'script!jquery/dist/jquery.min.js',
+'script!foundation-sites/dist/foundation.min.js',
   './app/app.jsx'
+],
+externals :{
+  jquery:'jQuery'
+},
+plugins: [
+  new webpack.ProvidePlugin  ({
+    '$':'jquery',
+    'jQuery':'jquery'
+  })
 ],
   output: {
     path:__dirname,
@@ -28,7 +38,7 @@ module.exports ={
       {
         loader:'babel-loader',
         query:{
-          presets: ['react','es2015','stage-0']
+          presets: ['react','es2015']
         },
         test:/\.jsx?$/,
         exclude:/(node_modules|bower_components)/
